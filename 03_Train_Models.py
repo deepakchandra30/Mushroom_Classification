e213135454f479186dc2a23da1388f809496bae7
deepakchandra30 <deepakchandra3012@gmail.com>
2025-06-08 14:45:51 +0100

    defined model training function

--- 03_Train_Models.py
+++ 03_Train_Models.py
@@ -1,4 +1,8 @@
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
 import joblib
 import pandas as pd
 
@@ -10,6 +14,35 @@ def load_data():
 
     return X_train,X_test,y_train,y_test
 
-X_train,X_test,y_train,y_test = load_data()
+def train_the_models(X_train, X_test,y_train,y_test):
+    models = {
+        "Logistic Regression" : LogisticRegression(max_iter=1000),
+        "Random Forest": RandomForestClassifier(n_estimators=100, random_state=42),
+        "Naive Bayes" : GaussianNB(),
+        "Decision Tree" : DecisionTreeClassifier(random_state=42)
+    }
 
-print(X_train.shape)
+    results = {}
+
+    for name,model in models.items():
+        model.fit(X_train, y_train)
+        predictions = model.predict(X_test)
+        acc = accuracy_score(y_test,predictions)
+        results[name] = acc
+        joblib.dump(model,f"files/{name.replace(' ','_')}.pkl")
+        print(f"{name} Accuracy: {acc:4f} ✅ Model saved to 'files/{name.replace(' ', '_')}.pkl'")
+
+    return results
+
+
+
+if __name__ == "__main__":
+    print("Loading train/test datasets...")
+    X_train,X_test,y_train,y_test = load_data()
+
+    print("🚀 Training the models and evaluating performance...")
+    accuracy_results = train_the_models(X_train,X_test,y_train,y_test)
+
+    print("\n📊 Model Comparison:")
+    for name, acc in accuracy_results.items():
+        print(f"{name}: {acc:.4f}")
