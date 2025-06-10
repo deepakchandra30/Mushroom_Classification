fafb7efd1607f74b81839c6e10427f5e5a08558a
deepakchandra30 <deepakchandra3012@gmail.com>
2025-06-10 16:50:25 +0100

    Loaded metrics and initiated request function

--- 04_app.py
+++ 04_app.py
@@ -14,3 +14,31 @@ models = {
     "Random Forest": joblib.load("files/Random_Forest.pkl"),
     "Decision Tree" : joblib.load("files/Decision_Tree.pkl")
 }
+
+def load_model_metrics(path="files/evaluations/model_metrics_report.csv"):
+    metrics= []
+    if os.path.exists(path):
+        df = pd.read_csv(path)
+        for _, row in df.iterrows():
+            metrics.append({
+                "model": row["Model"],
+                "accuracy": float(row["Accuracy"]),
+                "precision": float(row["Precision Score"]),
+                "recall": float(row["Recall"]),
+                "f1": float(row["F1 Score"])
+            })
+
+    return metrics  
+
+@app.route("/", methods = ["GET","POST"])
+def index():
+    prediction = None
+    model_outputs = {}
+    metrics = load_model_metrics()
+
+    if request.method == 'POST':
+        input_data = {}
+        for feature in label_endcoders:
+            continue
+        input_data[feature] = request.form.get(feature)
+         
