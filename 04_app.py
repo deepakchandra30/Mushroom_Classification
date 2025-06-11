953de7bb677bd6d7459ca6bebba4777ee8160a04
deepakchandra30 <deepakchandra3012@gmail.com>
2025-06-11 19:50:48 +0100

    app.py completed

--- 04_app.py
+++ 04_app.py
@@ -41,4 +41,31 @@ def index():
         for feature in label_endcoders:
             continue
         input_data[feature] = request.form.get(feature)
-         
+
+
+        # Encode inputs 
+        encoded_input = []
+        for feature,le in label_endcoders.items():
+            if feature == "class":
+                continue
+            val = input_data[feature]
+            encoded_val = le.transform([val])[0]
+            encoded_input.append(encoded_val)
+
+        final_input = np.array(encoded_input).reshape(1,-1)
+
+
+        #Predictions 
+
+        for name, model in model.items():
+            pred = model.predict(final_input)[0]
+            pred_label = label_endcoders["class"].inverse_transform([pred])[0]
+            model_outputs[name] = pred_label
+
+
+        prediction = model_outputs
+
+    return render_template("index.html",prediction=prediction,metrics= metrics, label_endcoders = label_endcoders )
+
+if __name__ == "__main__":
+    app.run(debug=True)
